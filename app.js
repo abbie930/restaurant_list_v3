0e4a3a1a4828496a80f5ca3ac99b549b5802cdff
abbie930 <abbiecheng930@gmail.com>
2022-12-23 16:03:08 +0800

    style: app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 // require packages used in the project
 const express = require('express')
-
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -12,15 +10,12 @@ if (process.env.NODE_ENV !== 'production') {
 
 const routes = require('./routes')
 require('./config/mongoose')
-
 const app = express()
 const port = 3000
 
 
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
-
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
