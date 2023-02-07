cd4dbc4b1b8a0b212d471411532ff5eaef890d51
abbie930 <abbiecheng930@gmail.com>
2023-02-07 23:25:35 +0800

    feat: add second seeduser data

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -8,52 +8,36 @@ const User = require('../user')
 const restaurantList = require('./restaurant.json').results
 const db = require('../../config/mongoose')
 
-// const SEED_USER = [
-// {
-//   name: 'user1',
-//   email: 'user1@example.com',
-//   password: '12345678'
-// },
-// { name: 'user2',
-//   email: 'user2@example.com',
-//   password: '12345678'
-// }
-// ]
-
-const SEED_USER =
-{
-  name: 'user1',
-  email: 'user1@example.com',
-  password: '12345678',
-  restaurantIndex: [0, 1, 2],
-}
-
+const SEED_USERS = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678',
+    restaurantIndex: [0, 1, 2],
+  },
+  { name: 'user2', email: 'user2@example.com', password: '12345678', restaurantIndex: [3, 4, 5] },
+]
 
 db.once('open', async () => {
   try {
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(SEED_USER.password, salt)
-    const user = await User.create({
-      name: SEED_USER.name,
-      email: SEED_USER.email,
-      password: hashedPassword,
-    })
-    const userId = user._id
-    await Promise.all(
-      Array.from({ length: 3 }, (_, i) =>
-        Restaurant.create({ ...restaurantList[SEED_USER.restaurantIndex[i]], userId })
+    for (const seedUser of SEED_USERS) {
+      const salt = await bcrypt.genSalt(10)
+      const hashedPassword = await bcrypt.hash(seedUser.password, salt)
+      const user = await User.create({
+        name: seedUser.name,
+        email: seedUser.email,
+        password: hashedPassword,
+      })
+      const userId = user._id
+      await Promise.all(
+        Array.from({ length: 3 }, (_, i) =>
+          Restaurant.create({ ...restaurantList[seedUser.restaurantIndex[i]], userId })
+        )
       )
-    )
+    }
     console.log('done')
     process.exit()
-  } 
-  catch (err) {
+  } catch (err) {
     console.log(err)
   }
 })
-
- //   for(let i = 0; i < restaurantList.length; i++) {
-  //   console.log(`add id: ${restaurantList[i]['id']} to mongodb`)
-  //   Restaurant.create({ ...restaurantList[i], userId })
-  // } 
-
