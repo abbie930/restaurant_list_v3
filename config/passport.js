a45a9fb350c5fc62400efad135ba6e80c75b9ec5
abbie930 <abbiecheng930@gmail.com>
2023-02-08 19:33:50 +0800

    fix: remove return in async await

--- config/passport.js
+++ config/passport.js
@@ -14,16 +14,16 @@ module.exports = (app) => {
       try {
         const user = await User.findOne({ email })
         if (!user) {
-          return done(null, false, req.flash('failure_msg', 'That email is not registered!'))
+          done(null, false, req.flash('failure_msg', 'That email is not registered!'))
         }
         const isMatch = await bcrypt.compare(password, user.password)
         if (!isMatch) {
-          return done(null, false, req.flash('failure_msg', 'Email or Password incorrect.'))
+          done(null, false, req.flash('failure_msg', 'Email or Password incorrect.'))
         } else {
-          return done(null, user)
+          done(null, user)
         }
       } catch (err) {
-        return done(err, false)
+        done(err, false)
       }
     })
   )
@@ -42,7 +42,7 @@ module.exports = (app) => {
           const { name, email } = profile._json
           const user = await User.findOne({ email })
           if (user) {
-            return done(null, user)
+            done(null, user)
           } else {
             const randomPassword = Math.random().toString(36).slice(-8)
             const salt = await bcrypt.genSalt(10)
@@ -52,24 +52,24 @@ module.exports = (app) => {
               email,
               password: hashedPassword,
             })
-            return done(null, newUserFromFb)
+            done(null, newUserFromFb)
           }
         } catch (err) {
-          return done(err, false, req.flash('failure_msg', 'Facebook Verification Failed'))
+          done(err, false, req.flash('failure_msg', 'Facebook Verification Failed'))
         }
       }
     )
   )
 
   passport.serializeUser(async (user, done) => {
-    return done(null, user.id)
+    done(null, user.id)
   })
   passport.deserializeUser(async (id, done) => {
     try {
       const user = await User.findById(id).lean()
-      return done(null, user)
+      done(null, user)
     } catch (err) {
-      return done(err, null)
+      done(err, null)
     }
   })
 }
