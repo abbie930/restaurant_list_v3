6b554be95a23f7e825f81a1646c6600d1a5390cb
abbie930 <abbiecheng930@gmail.com>
2023-02-11 17:03:01 +0800

    feat: add form validation for new create or edit

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
+const { validateFormInput } = require('../../models/validateFormInput')
 
 router.get('/new', (req, res) => {
   return res.render('new')
@@ -20,8 +21,24 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const userId = req.user._id
+    const errors = []
+    const validateResult = validateFormInput(req.body)
+    const formIsInvalidate = Object.values(validateResult).includes(false)
+    if (formIsInvalidate) {
+      if (!validateResult.phone) {
+        errors.push({ message: 'Phone input is invalid. (ex. 02-2222-2222)' })
+      }
+      if (!validateResult.google_map) {
+        errors.push({ message: 'Google_map URL is invalid. ex. http(s)://...' })
+      }
+      if (!validateResult.image) {
+        errors.push({ message: 'Image URL is invalid. ex. http(s)://...' })
+      }
+      return res.render('new', { restaurant: req.body, errors })
+    }
+
     const newData = req.body
+    const userId = req.user._id
     const restaurant = await Restaurant.create({ ...newData, userId })
     const id = restaurant._id
     res.redirect(`/restaurants/${id}`)
@@ -46,6 +63,21 @@ router.put('/:id', async (req, res) => {
     const userId = req.user._id
     const _id = req.params.id
     const newData = req.body
+    const errors = []
+    const validateResult = validateFormInput(newData)
+    const formIsInvalidate = Object.values(validateResult).includes(false)
+    if (formIsInvalidate) {
+      if (!validateResult.phone) {
+        errors.push({ message: 'Phone input is invalid. (ex. 02-2222-2222)' })
+      }
+      if (!validateResult.google_map) {
+        errors.push({ message: 'Google_map URL is invalid. ex. http(s)://...' })
+      }
+      if (!validateResult.image) {
+        errors.push({ message: 'Image URL is invalid. ex. http(s)://...' })
+      }
+      return res.render('new', { restaurant: newData, errors })
+    }
     await Restaurant.findOneAndUpdate({ _id, userId }, { ...newData, userId })
     res.redirect(`/restaurants/${_id}`)
   } catch (err) {
